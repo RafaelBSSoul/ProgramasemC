80bf4e48f6423d00277eb1123200afb8dbf325a2
RAFAEL BARBOSA <coffeemonstersandbooks@gmail.com>
2024-12-11 23:58:08 -0300

    Add files via upload

---
+++ ProjetoemC/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int soma(int a,int b){
+return a+b;
+}
+
+int main() {
+    int numero1 = 2;
+    int numero2 = 1;
+    int somat = numero1 + numero2;
+    printf("A soma é: %d\n", somat);
+    if(numero2<numero1){
+        printf("è maior que o numero \n");
+    } else{
+                printf("è maior ");
+
+    }
+
+soma(2,5);
+printf("%d\n", soma);
+    return 0;
+}
+
